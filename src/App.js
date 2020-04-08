a372b52976e2fe9e4d620320aafc32d19356f9db
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2020-04-08 13:49:03 +0300

    ra-6-1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import './bootstrap.min.css';
+import WorldClocks from './component/WorldClocks.js';
+
+function App() {
+  return (
+    <div className="container">
+      <WorldClocks />
+    </div>
+  );
+}
+
+export default App;
